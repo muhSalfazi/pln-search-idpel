baff7316f0546c15bfc184aa714688d6bba8b557
muhamad salman fauzi <salmanfauzi0512@gmail.com>
2024-12-31 00:10:53 +0700

    fix:updateusers

--- app/Http/Controllers/Users/UsersCT.php
+++ app/Http/Controllers/Users/UsersCT.php
@@ -1,14 +1,103 @@
 <?php
 
-namespace App\Http\Controllers\users;
+namespace App\Http\Controllers\Users;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\User;
+use Yajra\DataTables\Facades\DataTables;
+
 
 class UsersCT extends Controller
 {
-  public function index()
+  public function index(Request $request)
   {
-    return view('content.Users.users');
+    if ($request->ajax()) {
+      $totalData = User::count();
+      $totalFiltered = $totalData;
+
+      $limit = $request->input('length') ?: 10;
+      $start = $request->input('start') ?: 0;
+      $orderColumn = $request->input('order.0.column', 1);
+      $orderDir = $request->input('order.0.dir', 'asc');
+
+      $columns = [
+        1 => 'username',
+        2 => 'email',
+        3 => 'alamat',
+        4 => 'no_telp',
+        5 => 'jabatan'
+      ];
+
+      $order = $columns[$orderColumn] ?? 'created_at';
+
+      $query = User::query();
+
+      // Pencarian
+      if ($search = $request->input('search.value')) {
+        $query->where('username', 'LIKE', "%{$search}%")
+          ->orWhere('email', 'LIKE', "%{$search}%")
+          ->orWhere('alamat', 'LIKE', "%{$search}%")
+          ->orWhere('no_telp', 'LIKE', "%{$search}%")
+          ->orWhere('jabatan', 'LIKE', "%{$search}%");
+        $totalFiltered = $query->count();
+      }
+
+      $users = $query->offset($start)
+        ->limit($limit)
+        ->orderBy($order, $orderDir)
+        ->get();
+
+      $data = [];
+      $fakeId = $start + 1;
+
+      foreach ($users as $user) {
+        $nestedData['fake_id'] = $fakeId++;
+        $nestedData['username'] = $user->username ?? '-';
+        $nestedData['email'] = $user->email ?? '-';
+        $nestedData['alamat'] = $user->alamat ?? '-';
+        $nestedData['no_telp'] = $user->no_telp ?? '-';
+        $nestedData['jabatan'] = $user->jabatan ?? '-';
+        $data[] = $nestedData;
+      }
+
+      return response()->json([
+        'draw' => intval($request->input('draw')),
+        'recordsTotal' => $totalData,
+        'recordsFiltered' => $totalFiltered,
+        'data' => $data
+      ]);
+    }
+
+    return view('content.Users.Users');
   }
+
+  // Mengambil data untuk DataTables
+  public function getData(Request $request)
+  {
+    // Query hanya kolom tertentu
+    $query = User::select('username', 'email', 'alamat', 'no_telp', 'jabatan')
+      ->orderBy('created_at', 'desc');
+
+    return DataTables::of($query)
+      ->addIndexColumn()
+      ->editColumn('username', function ($user) {
+        return $user->username ?? '<span class="text-muted">Tidak ditemukan</span>';
+      })
+      ->editColumn('email', function ($user) {
+        return $user->email ?? '<span class="text-muted">-</span>';
+      })
+      ->editColumn('alamat', function ($user) {
+        return $user->alamat ?? '<span class="text-muted">-</span>';
+      })
+      ->editColumn('no_telp', function ($user) {
+        return $user->no_telp ?? '<span class="text-muted">-</span>';
+      })
+      ->editColumn('jabatan', function ($user) {
+        return $user->jabatan ?? '<span class="text-muted">-</span>';
+      })
+      ->rawColumns(['username', 'email', 'alamat', 'no_telp', 'jabatan'])
+      ->make(true);
+  }
+
 }
--- resources/views/content/Users/Users.blade.php
+++ resources/views/content/Users/Users.blade.php
@@ -1,139 +1,47 @@
 @extends('layouts/contentNavbarLayout')
 
-@section('title', 'users')
+@section('title', 'Users')
+
+
+@section('page-style')
+<!-- Tambahkan Bootstrap dan DataTables CSS -->
+<link rel="stylesheet" href="https://cdn.datatables.net/1.13.4/css/dataTables.bootstrap5.min.css">
+<link rel="stylesheet" href="https://cdn.datatables.net/responsive/2.5.0/css/responsive.bootstrap5.min.css">
+@endsection
+
+@section('vendor-script')
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.13.4/js/dataTables.bootstrap5.min.js"></script>
+<script src="https://cdn.datatables.net/responsive/2.5.0/js/dataTables.responsive.min.js"></script>
+<script src="https://cdn.datatables.net/responsive/2.5.0/js/responsive.bootstrap5.min.js"></script>
+@endsection
+
+@section('page-script')
+<script>
+  var baseUrl = "{{ url('/') }}/";
+</script>
+@vite('resources/assets/js/users.js')
+@endsection
 
 @section('content')
 <!-- Bordered Table -->
 <div class="card">
-  <h5 class="card-header">Data User</h5>
+  <h5 class="card-header">Data Users</h5>
   <div class="card-body">
     <div class="table-responsive text-nowrap">
-      <table class="table table-bordered">
+      <table id="usersTable" class="table table-bordered text-center align-middle">
         <thead>
           <tr>
-            <th>Nama</th>
+            <th>No</th>
+            <th>Name</th>
             <th>Email</th>
             <th>Alamat</th>
             <th>No Telp</th>
             <th>Jabatan</th>
           </tr>
         </thead>
-        <tbody>
-
-          <tr>
-            <td><i class="bx bxl-angular bx-md text-danger me-4"></i> <span>Angular Project</span></td>
-            <td>Albert Cook</td>
-            <td>
-              <ul class="list-unstyled m-0 avatar-group d-flex align-items-center">
-                <li data-bs-toggle="tooltip" data-popup="tooltip-custom" data-bs-placement="top" class="avatar avatar-xs pull-up" title="Lilian Fuller">
-                  <img src="../assets/img/avatars/5.png" alt="Avatar" class="rounded-circle">
-                </li>
-                <li data-bs-toggle="tooltip" data-popup="tooltip-custom" data-bs-placement="top" class="avatar avatar-xs pull-up" title="Sophia Wilkerson">
-                  <img src="../assets/img/avatars/6.png" alt="Avatar" class="rounded-circle">
-                </li>
-                <li data-bs-toggle="tooltip" data-popup="tooltip-custom" data-bs-placement="top" class="avatar avatar-xs pull-up" title="Christina Parker">
-                  <img src="../assets/img/avatars/7.png" alt="Avatar" class="rounded-circle">
-                </li>
-              </ul>
-            </td>
-            <td><span class="badge bg-label-primary me-1">Active</span></td>
-            <td>
-              <div class="dropdown">
-                <button type="button" class="btn p-0 dropdown-toggle hide-arrow" data-bs-toggle="dropdown"><i class="bx bx-dots-vertical-rounded"></i></button>
-                <div class="dropdown-menu">
-                  <a class="dropdown-item" href="javascript:void(0);"><i class="bx bx-edit-alt me-1"></i> Edit</a>
-                  <a class="dropdown-item" href="javascript:void(0);"><i class="bx bx-trash me-1"></i> Delete</a>
-                </div>
-              </div>
-            </td>
-          </tr>
-
-
-          <tr>
-            <td><i class="bx bxl-react bx-md text-info me-4"></i> <span>React Project</span></td>
-            <td>Barry Hunter</td>
-            <td>
-              <ul class="list-unstyled m-0 avatar-group d-flex align-items-center">
-                <li data-bs-toggle="tooltip" data-popup="tooltip-custom" data-bs-placement="top" class="avatar avatar-xs pull-up" title="Lilian Fuller">
-                  <img src="../assets/img/avatars/5.png" alt="Avatar" class="rounded-circle">
-                </li>
-                <li data-bs-toggle="tooltip" data-popup="tooltip-custom" data-bs-placement="top" class="avatar avatar-xs pull-up" title="Sophia Wilkerson">
-                  <img src="../assets/img/avatars/6.png" alt="Avatar" class="rounded-circle">
-                </li>
-                <li data-bs-toggle="tooltip" data-popup="tooltip-custom" data-bs-placement="top" class="avatar avatar-xs pull-up" title="Christina Parker">
-                  <img src="../assets/img/avatars/7.png" alt="Avatar" class="rounded-circle">
-                </li>
-              </ul>
-            </td>
-            <td><span class="badge bg-label-success me-1">Completed</span></td>
-            <td>
-              <div class="dropdown">
-                <button type="button" class="btn p-0 dropdown-toggle hide-arrow" data-bs-toggle="dropdown"><i class="bx bx-dots-vertical-rounded"></i></button>
-                <div class="dropdown-menu">
-                  <a class="dropdown-item" href="javascript:void(0);"><i class="bx bx-edit-alt me-1"></i> Edit</a>
-                  <a class="dropdown-item" href="javascript:void(0);"><i class="bx bx-trash me-1"></i> Delete</a>
-                </div>
-              </div>
-            </td>
-          </tr>
-
-
-          <tr>
-            <td><i class="bx bxl-vuejs bx-md text-success me-4"></i> <span>VueJs Project</span></td>
-            <td>Trevor Baker</td>
-            <td>
-              <ul class="list-unstyled m-0 avatar-group d-flex align-items-center">
-                <li data-bs-toggle="tooltip" data-popup="tooltip-custom" data-bs-placement="top" class="avatar avatar-xs pull-up" title="Lilian Fuller">
-                  <img src="../assets/img/avatars/5.png" alt="Avatar" class="rounded-circle">
-                </li>
-                <li data-bs-toggle="tooltip" data-popup="tooltip-custom" data-bs-placement="top" class="avatar avatar-xs pull-up" title="Sophia Wilkerson">
-                  <img src="../assets/img/avatars/6.png" alt="Avatar" class="rounded-circle">
-                </li>
-                <li data-bs-toggle="tooltip" data-popup="tooltip-custom" data-bs-placement="top" class="avatar avatar-xs pull-up" title="Christina Parker">
-                  <img src="../assets/img/avatars/7.png" alt="Avatar" class="rounded-circle">
-                </li>
-              </ul>
-            </td>
-            <td><span class="badge bg-label-info me-1">Scheduled</span></td>
-            <td>
-              <div class="dropdown">
-                <button type="button" class="btn p-0 dropdown-toggle hide-arrow" data-bs-toggle="dropdown"><i class="bx bx-dots-vertical-rounded"></i></button>
-                <div class="dropdown-menu">
-                  <a class="dropdown-item" href="javascript:void(0);"><i class="bx bx-edit-alt me-1"></i> Edit</a>
-                  <a class="dropdown-item" href="javascript:void(0);"><i class="bx bx-trash me-1"></i> Delete</a>
-                </div>
-              </div>
-            </td>
-          </tr>
-
-          <tr>
-            <td><i class="bx bxl-bootstrap bx-md text-primary me-4"></i> <span>Bootstrap Project</span></td>
-            <td>Jerry Milton</td>
-            <td>
-              <ul class="list-unstyled m-0 avatar-group d-flex align-items-center">
-                <li data-bs-toggle="tooltip" data-popup="tooltip-custom" data-bs-placement="top" class="avatar avatar-xs pull-up" title="Lilian Fuller">
-                  <img src="../assets/img/avatars/5.png" alt="Avatar" class="rounded-circle">
-                </li>
-                <li data-bs-toggle="tooltip" data-popup="tooltip-custom" data-bs-placement="top" class="avatar avatar-xs pull-up" title="Sophia Wilkerson">
-                  <img src="../assets/img/avatars/6.png" alt="Avatar" class="rounded-circle">
-                </li>
-                <li data-bs-toggle="tooltip" data-popup="tooltip-custom" data-bs-placement="top" class="avatar avatar-xs pull-up" title="Christina Parker">
-                  <img src="../assets/img/avatars/7.png" alt="Avatar" class="rounded-circle">
-                </li>
-              </ul>
-            </td>
-            <td><span class="badge bg-label-warning me-1">Pending</span></td>
-            <td>
-              <div class="dropdown">
-                <button type="button" class="btn p-0 dropdown-toggle hide-arrow" data-bs-toggle="dropdown"><i class="bx bx-dots-vertical-rounded"></i></button>
-                <div class="dropdown-menu">
-                  <a class="dropdown-item" href="javascript:void(0);"><i class="bx bx-edit-alt me-1"></i> Edit</a>
-                  <a class="dropdown-item" href="javascript:void(0);"><i class="bx bx-trash me-1"></i> Delete</a>
-                </div>
-              </div>
-            </td>
-          </tr>
-        </tbody>
+        <tbody></tbody>
       </table>
     </div>
   </div>
